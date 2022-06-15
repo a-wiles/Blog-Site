0a82ce6ee3ba9cd241dbffab27ceb3bee705f501
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-15 19:15:12 -0400

    Set up connection and schema

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3001
+});
+
+module.exports = sequelize;
