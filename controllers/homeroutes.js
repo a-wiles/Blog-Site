5c255c40b2c5a076bdb36663a4025f7019a1062b
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-21 23:14:00 -0400

    Adjusted the homeroutes

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,57 +3,98 @@ const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    const dbUserData = await User.findAll({
-      include: [
-        {
-          model: Post,
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_content",
+      "title",
+      "created_at"
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
         },
-      ],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
+});
 
-    const userData = dbUserData.map((post) =>
-      post.get({ plain: true })
-    );
+router.get("/post/:id", withAuth, (req, res) => {
 
-    console.log(userData);
-    res.render('homepage', {
-      userData,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_content',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: [
+          'id',
+          'comment_text',
+          'post_id',
+          'user_id',
+          'created_at'
+        ],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      },
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      }
+    ]
+  })
+  .then(dbPostData => {
+    if(!dbPostData) {
+      res.status(404).json({ message: 'No post found' });
+      return;
+    }
+    const post = dbPostData.get({plain: true});
+    res.render('single-post', { post, loggedIn: true });
+  })
+  .catch(err => {
     console.log(err);
     res.status(500).json(err);
-  }
+  });
 });
 
 
-router.get('/dashboard', withAuth, async (req, res) => {
-  try {
-    const dbUserData = await User.findByPk(req.session.user_id, {
-      include: [
-        {
-          model: Post,
-        },
-      ],
-    });
-    
-    const userData = dbUserData.get({ plain: true });
-    res.render('dashboard', { userData, loggedIn: req.session.loggedIn });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
-
-  res.render('login');
+  res.render("/");
 });
+
 
 module.exports = router;
