4387bda7f8ba3a1743ba2db58c7404d29533e815
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-06-21 23:00:26 -0400

    Fixed error and added module export

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const commentRoutes = require('./comment-routes');
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
